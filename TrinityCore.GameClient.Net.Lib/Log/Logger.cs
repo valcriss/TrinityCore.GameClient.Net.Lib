2e67a310388cd3c41c92337285a61c38304e2c99
Daniel Silvestre <danielsilvestre.work@gmail.com>
2021-09-09 17:27:08 +0200

    Create Logger.cs

---
+++ TrinityCore.GameClient.Net.Lib/Log/Logger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrinityCore.GameClient.Net.Lib.Log
+{
+    public static class Logger
+    {
+        public static ILogger LogObject { get; set; }
+        public static LogLevel Level { get; set; }
+
+        static Logger()
+        {
+            Level = LogLevel.WARNING;
+        }
+
+        public static void LogException(Exception e)
+        {
+            LogObject?.LogException(e);
+        }
+
+        public static void Log(string message, LogLevel level = LogLevel.INFO)
+        {
+            if ((int)level <= (int)Level)
+            {
+                LogObject?.Log(message, level);
+            }
+        }
+    }
+
+    public interface ILogger
+    {
+        void LogException(Exception e);
+        void Log(string message, LogLevel level = LogLevel.INFO);
+    }
+
+    public enum LogLevel
+    {
+        DETAIL = 4,
+        INFO = 3,
+        WARNING = 2,
+        ERROR = 0,
+        SUCCESS = 1,
+    }
+}
