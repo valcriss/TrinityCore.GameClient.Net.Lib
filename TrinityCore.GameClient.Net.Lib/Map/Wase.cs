8b710609b8c0eca1e2f0677a8fb3358eb7807d34
Daniel Silvestre <danielsilvestre.work@gmail.com>
2021-09-20 01:06:50 +0200

    Create Wase.cs

---
+++ TrinityCore.GameClient.Net.Lib/Map/Wase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using TrinityCore.Map.Net.IO;
+
+namespace TrinityCore.GameClient.Net.Lib.Map
+{
+    public class Wase
+    {
+
+        private static Wase _instance;
+        public static Wase Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Wase();
+                }
+                return _instance;
+            }
+        }
+
+        private string MmapDirectory { get; set; }
+        private MMapTileReader TileReader { get; set; }
+
+        public Wase()
+        {
+
+        }
+
+        public bool Initialize(string mmapDirectory)
+        {
+            if (!System.IO.Directory.Exists(MmapDirectory) || !System.IO.File.Exists(System.IO.Path.Combine(MmapDirectory, "000.mmap")))
+            {
+                return false;
+            }
+
+            MmapDirectory = mmapDirectory;
+            TileReader = new MMapTileReader(MmapDirectory);
+
+            return true;
+        }
+
+        public Vector3 GetNearestPolyCenter(int mapId, Vector3 position)
+        {
+            MmapTileFile tile = TileReader.GetTile(mapId, position);
+            
+        }
+    }
+}
