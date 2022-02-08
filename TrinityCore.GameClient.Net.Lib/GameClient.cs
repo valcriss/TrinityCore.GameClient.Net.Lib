d5415c33c18900323b9deded8e286d0242a9d676
Daniel Silvestre <danielsilvestre.work@gmail.com>
2022-02-08 17:10:35 +0100

    update

---
+++ TrinityCore.GameClient.Net.Lib/GameClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrinityCore.GameClient.Net.Lib.Network.Auth.Models;
+using TrinityCore.GameClient.Net.Lib.Network.World.Models;
+
+namespace TrinityCore.GameClient.Net.Lib
+{
+    class GameClient : IDisposable
+    {
+        public GameClient(string username, string password, string hostname, int port = 3724)
+        {
+
+        }
+
+        public bool Authenticate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<WorldServerInfo> GetRealms()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ConnectToRealm(WorldServerInfo realm)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Character> GetCharacters()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool EnterRealm(Character character)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
